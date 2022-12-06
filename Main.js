349af72f2695f6e4a1bf7d999ffa59aa207640c8
Apar Tyagi <apartyagi10@gmail.com>
2022-12-06 13:26:47 +0530

    render push

--- Main.js
+++ Main.js
@@ -2,7 +2,7 @@ import express from 'express';
 import userRoute from './src/app/routes/userRoutes';
 import "./src/config/mongodb";
 const app=express();
-const port =3000;
+const port =process.env.PORT || 3000;
 
 
 app.use(express.json());
